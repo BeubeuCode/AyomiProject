6f47c8ad0920b02a7d9af4f1377354385ee5eee5
Benoît A <benoit.arnlt@gmail.com>
2024-01-07 18:05:20 +0100

    worplace

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ def index():
     return {'status': 'OK'}
 
 
+@app.get('/compute/')
+def compute_get():
+    return {'message': 'use POST method to compute'}
+
 @app.post('/compute/')
 def compute(request: ComputeRequest):
     return {'result': reverse_polish_notation_calculator_instance.calculate(request.calculation)}
