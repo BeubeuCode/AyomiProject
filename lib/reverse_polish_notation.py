20c4c3e9276133bdaa71d7b1e7f690f471c16b06
Benoît A <benoit.arnlt@gmail.com>
2024-01-07 18:14:53 +0100

    fix RPN class

--- lib/reverse_polish_notation.py
+++ lib/reverse_polish_notation.py
@@ -9,13 +9,26 @@ class ReversePolishNotation:
         return self.stack.pop()
 
     def calculate(self, expression):
-        for item in expression:
-            if item in ['+', '-', '*', '/']:
-                second = self.pop()
-                first = self.pop()
-                self.push(eval(str(first) + item + str(second)))
-            else:
-                self.push(item)
-        return self.pop()
+        operators = {'+': lambda x, y: x + y,
+                     '-': lambda x, y: x - y,
+                     '*': lambda x, y: x * y,
+                     '/': lambda x, y: x / y}
 
-reverse_polish_notation_calculator_instance = ReversePolishNotation()
+        for token in expression.split():
+            if token.isdigit():
+                self.stack.append(float(token))
+            elif token in operators:
+                if len(self.stack) < 2:
+                    return "Invalid expression: Not enough operands for operator"
+                else:
+                    operand2 = self.stack.pop()
+                    operand1 = self.stack.pop()
+                    result = operators[token](operand1, operand2)
+                    self.stack.append(result)
+            else:
+                return "Invalid token: " + token
+
+        if len(self.stack) != 1:
+            return "Invalid expression: Too many operands"
+
+        return self.stack[0]
