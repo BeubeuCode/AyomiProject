295993fbbacc6fd17ba91fd7b930ab1806e561ff
Benoît A <benoit.arnlt@gmail.com>
2024-01-14 21:17:46 +0100

    database handling + results fetching

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+from csv import writer as csv_writer
+from io import StringIO, BytesIO
+
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 from lib.reverse_polish_notation import ReversePolishNotation
-
+from lib.db import Database
 
 app = FastAPI()
+
+db = Database('calculator.db')
 
 class ComputeRequest(BaseModel):
     calculation: str
@@ -21,5 +27,34 @@ def compute_get():
 @app.post('/compute/')
 def compute(request: ComputeRequest):
     rpn = ReversePolishNotation()
-    return {'result': rpn.calculate(request.calculation)}
+    result = rpn.calculate(request.calculation)
 
+    db.insert_result(request.calculation, result)
+
+    return {'result': result}
+
+@app.get('/results/')
+def results():
+    results = db.get_all_results()
+    return {'results': results}
+
+@app.get('/results/csv')
+async def results_as_csv():
+    results = db.get_all_results()
+
+    output = StringIO()
+    writer = csv_writer(output)
+    writer.writerow(['id', 'calculation', 'result'])
+
+    for result in results:
+        writer.writerow(result)
+
+    output.seek(0)
+
+    return StreamingResponse(BytesIO(output.read().encode('utf-8')), media_type="text/csv", headers={'Content-Disposition': 'attachment; filename="results.csv"'})
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    db.disconnect()
+    pass
